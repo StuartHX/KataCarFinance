b26b1a7068d612c1633b313ab4ac3d7854a02bc0
StuartHX <31853723+StuartHX@users.noreply.github.com>
2017-09-11 14:44:10 +0100

    Add files via upload

---
+++ Carfinance.Phoenix.Kata.Angular/app/module.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('PhoenixKata', ['ngRoute'])
+
+        .controller('bookingController', function ($scope, $http) {
+            //declare variable for mainain ajax load and entry or edit mode
+            $scope.loading = true;
+            $scope.addMode = false;
+            $scope.viewMode = true;
+
+            //get all booking information
+            $http.get('booking').then(function (response) {
+                $scope.bookings = response.data;
+                $scope.loading = false
+            })
+            
+            //by pressing toggleEdit button ng-click in html, this method will be hit
+            $scope.toggleEdit = function () {
+                this.booking.editMode = !this.booking.editMode;
+            };
+
+            //by pressing toggleAdd button ng-click in html, this method will be hit
+            $scope.toggleAdd = function () {
+                $scope.addMode = !$scope.addMode;
+                $scope.viewMode = !$scope.viewMode;
+            };
+
+            $scope.add = function () {
+                $scope.loading = true;
+                var mydata = JSON.stringify(this.newbooking);
+                $http({
+                    url: 'booking',
+                    method: "POST",
+                    data: this.newbooking,
+                    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                }).then(function (response) {
+                    console.log('Successful Insert')  
+                    $http.get('booking').then(function (response) {
+                        $scope.bookings = response.data;
+                    });
+                    $scope.addMode = false;
+                    $scope.viewMode = true;
+                }); 
+            };
+
+            //Edit Booking
+            $scope.save = function () {
+                var editedBooking = this.booking;
+                $http({
+                    url: 'booking',
+                    method: "PUT",
+                    data: this.booking,
+                    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                }).then(function (response) {
+                    console.log("Booking updated successfully");
+                    editedBooking.editMode = false;
+                });           
+            };
+
+           
+
+            
+        });
+})();
